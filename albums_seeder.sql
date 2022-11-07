9d832b3f47d80bd3649ca0dbb0a00f87ef9e1833
Richard Kim <richardjkim7@gmail.com>
2022-11-07 11:59:00 -0600

    sql basic statements exericse complete

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -2,3 +2,7 @@ USE codeup_test_db;
 
 INSERT INTO album (artist, record_name, release_date, sales, genre)
 VALUES ('Celin Dion', 'Falling into You', 1996, 21.1, 'pop');
+
+INSERT INTO album (artist, record_name, release_date, sales, genre)
+VALUES ('Adele', '21', 2011, 27.1, 'pop');
+
---
+++ delete_exercises.sql
@@ -0,0 +1,5 @@
+USE codeup_test_db;
+
+SELECT * FROM album WHERE id = 5;
+
+DELETE FROM album WHERE id = 5;
