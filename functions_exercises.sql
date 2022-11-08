26cb9056bbf7f8865def2f18afbdc77fe841bbf5
Richard Kim <richardjkim7@gmail.com>
2022-11-08 15:49:07 -0600

    MySql functions exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -10,4 +10,4 @@ SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(bi
 # Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
 SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12   AND day(birth_date) = 25 ORDER BY hire_date DESC;
 # For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
-
+SELECT DATEDIFF(NOW(), hire_date) FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12   AND day(birth_date) = 25;
