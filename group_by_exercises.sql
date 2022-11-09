73012a87bbb485daaa6c2ec805918c2513ab7b51
Richard Kim <richardjkim7@gmail.com>
2022-11-09 10:03:32 -0600

    MySQL group by exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -4,8 +4,10 @@ USE employees;
 SELECT DISTINCT title FROM titles;
 
 # Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY. The results should be:
+SELECT DISTINCT last_name FROM employees WHERE last_name like 'E%' AND last_name like '%E';
 
 # Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+SELECT DISTINCT first_name, last_name FROM employees WHERE last_name like 'E%' AND last_name like '%E';
 
 # Find the unique last names with a 'q' but not 'qu'. Your results should be:
 
