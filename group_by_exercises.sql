b89fa23e0392b30bec6c63de5a7c54b2bb74dd15
Richard Kim <richardjkim7@gmail.com>
2022-11-09 09:56:27 -0600

    MySql group by exercises

---
+++ group_by_exercises.sql
@@ -0,0 +1,14 @@
+USE employees;
+
+# In your script, use DISTINCT to find the unique titles in the titles table. Your results should look like:
+SELECT DISTINCT title FROM titles;
+
+# Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY. The results should be:
+
+# Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+
+# Find the unique last names with a 'q' but not 'qu'. Your results should be:
+
+# Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+
+# Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names. Your results should be:
