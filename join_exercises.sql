bdbbf3a9e8fc7bc33e300a0a20b3667479e636f1
Richard Kim <richardjkim7@gmail.com>
2022-11-10 22:48:43 -0600

    joins exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -9,4 +9,19 @@ INSERT INTO users (id, name, email, role_id) VALUES
 
 # Use JOIN, LEFT JOIN, and RIGHT JOIN to combine results from the users and roles tables as we did in the lesson. Before you run each query, guess the expected number of results.
 
+# JOIN
+SELECT users.name as user_name, roles.name as role_name
+FROM users
+JOIN roles ON users.role_id = roles.id;
+
+# LEFT JOIN - all users and jobs or no jobs listed
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+LEFT JOIN roles ON users.role_id = roles.id;
+
+# RIGHT JOIN - user names without jobs are eliminated
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+RIGHT JOIN roles ON users.role_id = roles.id;
+
 # Although not explicitly covered in the lesson, aggregate functions like count can be used with join queries. Use COUNT and the appropriate join type to get a list of roles along with the number of users that have a given role. Hint: You will also need to use GROUP BY in the query.
